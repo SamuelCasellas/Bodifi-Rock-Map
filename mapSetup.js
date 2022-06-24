433e6bdf49e1b01982aa305b244454d2337affaf
Samuel Casellas <sam.casellas@gmail.com>
2022-06-24 11:27:43 -0600

    Added game boundary

--- mapSetup.js
+++ mapSetup.js
@@ -1,6 +1,7 @@
 const API_KEY = "AAPK321d61bd8cb64adcaf742d6cdb358ccdCXO2JrGUFiZl0PQvgiRgB1wGhZCRKBqE5xirtn9bSIMdlfdsRnmH33XiIBVB2JfN";
 
 const IDAHO_PARKS_FEATURE_LAYER_URL = "https://services8.arcgis.com/YY0zev9RpFX809lW/arcgis/rest/services/Parks_in_IF/FeatureServer/0";
+const GAME_BOUNDRY_FEATURE_LAYER_URL = "https://services8.arcgis.com/YY0zev9RpFX809lW/arcgis/rest/services/Game_Boundry/FeatureServer/0";
 
 // Rigby is the between Rexburg and Idaho so we will use that.
 const RIGBY_LONGITUDE = -111.909829694;
@@ -29,6 +30,7 @@ require([
         container: "viewDiv" // Div element
     });
 
+    // Configure parks
     const popupPark = {
         "title": "Parks",
         "content": "<b>Park name:</b> {Park_Name}<br>\
@@ -45,5 +47,27 @@ require([
     });
 
     map.add(parks);
+
+    // Add Game Boundaries
+    const gameBoundarySketch = new FeatureLayer({
+        url: GAME_BOUNDRY_FEATURE_LAYER_URL
+    });
+
+    map.add(gameBoundarySketch);
+    
+    
+    // Control altitude, game boundary
+    const boundaryButton = document.getElementById("boundary");
+    let boundaryShow = true;
+    
+    boundaryButton.addEventListener("click", function() {
+        if (boundaryShow) {
+            map.remove(gameBoundarySketch);
+            boundaryShow = false;
+        } else {
+            map.add(gameBoundarySketch);
+            boundaryShow = true;
+        }
+    });
 
 });
