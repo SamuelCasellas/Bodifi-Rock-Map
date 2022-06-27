63636e37a85bf92c54041f82d661928a5f2f5194
Samuel Casellas <sam.casellas@gmail.com>
2022-06-27 12:34:54 -0600

    Added public spaces and campus

--- README.md
+++ README.md
@@ -1,25 +1,67 @@
-# ArcGIS-Mapping
-Showcase all of the different parks in the Rexburg/Idaho Falls locations
+# Important dates
 
-# Overview
+3 mini rocks start date: Wednesday, June 29, 2022 (W11)
+Final rock start date: Monday, July 11, 2022 (W13)
 
-Here I decided to figure out how to use another rest api endpoint with a token accessed by ArcGIS to understand how to configure a map in the web.
+# Symbols on the shirt
+NW: Triangle ∆
+NE: hexagon with a dot in the middle
+ES: Three vetrical lines and one horizontal line
+WS: * with 12 dots surrounding it
 
-[Software Demo Video](https://youtu.be/5l7YD2yiit4)
+Middle of compass: an upside v with a dot between the legs
 
-# Development Environment
+# Things that will be helpful to know for the rock:
 
-I used the ArcGis library along with javascript and HTML to display the map in the browser.
+1. Places where the rock can actually be hiding:
+    - Forests
+    - ~~Parks~~
+        - ArcGIS
+            - https://chluyu7llsleoj1k.maps.arcgis.com/home/item.html?id=f092c20803a047cba81fbf1e30eff0b5
+        - General
+            - Idaho Falls https://www.idahofallsidaho.gov/Facilities?clear=False
+    - Any public spaces, really (they hid it at a library once!)
 
-# Useful Websites
+2. Nearby landmarks 
+    - Bodies of water
+    - Museums
+    - Churches
+    - Restaurants?
 
-(Make a list of websites that I found helpful in this project)
-* [ArcGis Website: Creating a feature Layer](https://developers.arcgis.com/documentation/mapping-apis-and-services/data-hosting/tutorials/tools/create-a-new-feature-layer/#add-data)
-* [Display a map](https://developers.arcgis.com/javascript/latest/display-a-map/)
-* [Idaho Falls Parks Map](https://www.idahofallsidaho.gov/Facilities?clear=False)
+3. Facts about the general area or hiding spot
+    - historical
+    - events currently being held (like a parade where theres food)
+    - Venues
+    - ~~Altitude (which parts are hilly)~~
 
-# Future Work
+4. (The rock WONT be hiding)
+    - Farms
+    - Homes
+    - Private Property (The Bodifi Rock will not be hidden within 50ft of any commercial building or residence.)
+    - Places of business
+    - "Keep off" signage
 
-* Add a filter for the markers based on altitude
-* Add some more feature layers
-* Have a legend that shows the different locations in IF, such as schools and churchers
+# Overarching ideas for program to help us find the rock
+Note: If I cannot find a way to show the different filters on my own ArcGIS app, I can always use this -> https://www.arcgis.com/apps/mapviewer/index.html
+
+- Implement ArcGIS to pinpoint all the parks/forests (1)
+- ArcGIS for any sort of public spaces (1)
+- ArcGIS for any nearby landmarks: Bodies of water, museums, churches, restaurants. (2)
+- ~~ArcGIS for altitude (3d)~~
+- ArcGIS for  places that are private property: they will not hide the rock there. (4)
+
+# How it will fit into the modules:
+4: ArcGIS (For displaying all of the feature layers as described above)
+5: A Webapp that will hold the ArcGIS app
+(I will then put it on glitch so we can use it on the run when finding the rock)
+
+# Researching program (simplicity is key)
+- Have all the feature layers implemented with appropriate popups. 
+- ~~Use a database (Module 5 stretch challenge) that will hold the facts about each location and display the results (like mod. 2) EASY: Already implemented in ArcGIS~~
+
+# Key components for the on-the-search program
+- Must be able to access it as a website (so use glitch)
+- With the database: store custom notes about each location to store progress in the hunt
+
+# How to utilize hints given at 8AM (Facebook and Instagram will also have the hints)
+- Run synonyms of key words against different locations' facts *
--- mapSetup.js
+++ mapSetup.js
@@ -1,13 +1,15 @@
 const API_KEY = "AAPK321d61bd8cb64adcaf742d6cdb358ccdCXO2JrGUFiZl0PQvgiRgB1wGhZCRKBqE5xirtn9bSIMdlfdsRnmH33XiIBVB2JfN";
 
-const IDAHO_PARKS_FEATURE_LAYER_URL = "https://services8.arcgis.com/YY0zev9RpFX809lW/arcgis/rest/services/Parks_in_IF/FeatureServer/0";
+const SLOPE_URL = "https://elevation.arcgis.com/arcgis/rest/services/WorldElevation/Terrain/ImageServer/";
+
 const GAME_BOUNDRY_FEATURE_LAYER_URL = "https://services8.arcgis.com/YY0zev9RpFX809lW/arcgis/rest/services/Game_Boundry/FeatureServer/0";
+const IDAHO_PARKS_FEATURE_LAYER_URL = "https://services8.arcgis.com/YY0zev9RpFX809lW/arcgis/rest/services/Parks_in_IF/FeatureServer/0";
+const PUBLIC_LAND_URL = "https://services1.arcgis.com/Hp6G80Pky0om7QvQ/arcgis/rest/services/PAD_US/FeatureServer";
+const CAMPUS_MAP_URL = "https://services1.arcgis.com/z5tlnpYHokW9isdE/arcgis/rest/services/IdahoFallsCurrentCampusMap/FeatureServer/0";
+
 
 // const southWest = [-112.182817, 43.304475];
 // const northEast = [-111.59577, 44.071117];
-
-const SLOPE_URL = "https://elevation.arcgis.com/arcgis/rest/services/WorldElevation/Terrain/ImageServer/";
-
 
 // Rigby is the between Rexburg and Idaho so we will use that.
 const RIGBY_LONGITUDE = -111.909829694;
@@ -41,12 +43,13 @@ require([
     // Configure altitude
     const altitude = new ImageryLayer({
         portalItem: {
-            id: 'a1ba14d09df14f42ad6ca3c4bcebf3b4'
+            id: "a1ba14d09df14f42ad6ca3c4bcebf3b4"
         }
     });
 
     altitude.opacity = 0.7;
     map.add(altitude, 0);
+    altitude.visible = false;
 
 
     // Add Game Boundaries
@@ -73,17 +76,110 @@ require([
     });
     
     map.add(parks, 2);
+
+    // Configure public spaces
+    const popupPublicSpace = {
+        "title": "Public Space",
+        "content": "<b>Place Name: </b> {Unit_Nm}<br></br>\
+        <b>Feature Class:</b> {FeatClass}<br></br>\
+        <b>Category:</b> {Category}<br></br>\
+        <b>Owner Type:</b> {Own_Type}<br></br>\
+        <b>Owner Name:</b> {Own_Name}<br></br>\
+        <b>Local Owner:</b> {Loc_Own}<br></br>\
+        <b>Manager Type:</b> {Mang_Type}<br></br>\
+        <b>Manager Name:</b> {Mang_Name}<br></br>\
+        <b>Local Manager:</b> {Loc_Mang}<br></br>\
+        <b>Designation Type:</b> {Des_Tp}<br></br>\
+        <b>Local Designation:</b> {Loc_Ds}<br></br>\
+        <b>Local Name:</b> {Loc_Nm}<br></br>\
+        <b>GIS Source Date:</b> {Src_Date}<br></br>\
+        <b>GIS Acres:</b> {GIS_Acres}<br></br>\
+        <b>Public Access:</b> {Pub_Access}<br></br>\
+        <b>Public Access Date:</b> {Access_Dt}<br></br>\
+        <b>GAP Status Code:</b> {GAP_Sts}<br></br>\
+        <b>Date of Establishment:</b> {Date_Est}<br></br>\
+        <b>Comments:</b> {Comments}<br></br>\
+        <b>Shape Area:</b> {Shape__Area}<br></br>\
+        <b>Shape Length:</b> {Shape__Length}<br></br>"
+    }
+
+    const publicSpaces = new FeatureLayer({
+        url: PUBLIC_LAND_URL,
+        outFields: [
+            "FeatClass",
+            "Category",
+            "Own_Type",
+            "Own_Name",
+            "Loc_Own",
+            "Mang_Type",
+            "Mang_Name",
+            "Loc_Mang",
+            "Des_Tp",
+            "Loc_Ds",
+            "Unit_Nm",
+            "Loc_Nm",
+            "Src_Date",
+            "GIS_Acres",
+            "Pub_Access",
+            "Access_Dt",
+            "GAP_Sts",
+            "Date_Est",
+            "Comments",
+            "Shape__Area",
+            "Shape__Length",
+        ],
+        popupTemplate: popupPublicSpace
+    });
+
+    publicSpaces.opacity = 0.5;
+    map.add(publicSpaces, 2);
     
-    
-    // Control altitude, game boundary
+
+    // Add Campus
+
+    const campus = new FeatureLayer({
+        url: CAMPUS_MAP_URL
+    });
+
+    campus.visible = false;
+    map.add(campus, 2);
+
+    // Toggle map features
     const altitudeButton = document.getElementById("altitude");
     altitudeButton.addEventListener("click", function() {
         altitude.visible = !(altitude.visible);
+        altitudeButton.value = "Show altitude: " + tellOnOrOff(altitude);
     });
 
     const boundaryButton = document.getElementById("boundary");
     boundaryButton.addEventListener("click", function() {
       gameBoundarySketch.visible = !(gameBoundarySketch.visible);
+      boundaryButton.value = "Show game boundary: " + tellOnOrOff(gameBoundarySketch);
     });
+
+    const parksButton = document.getElementById("parks");
+    parksButton.addEventListener("click", function() {
+      parks.visible = !(parks.visible);
+      parksButton.value = "Show park points: " + tellOnOrOff(parks);
+    });
+
+    const publicSpacesButton = document.getElementById("public-land");
+    publicSpacesButton.addEventListener("click", function() {
+      publicSpaces.visible = !(publicSpaces.visible);
+      publicSpacesButton.value = "Show public lands: " + tellOnOrOff(publicSpaces);
+    });
+
+    const campusButton = document.getElementById("campus");
+    campusButton.addEventListener("click", function() {
+      campus.visible = !(campus.visible);
+      campusButton.value = "Show Idaho Campus: " + tellOnOrOff(campus);
+    });
+
+    function tellOnOrOff(feature) {
+        if (feature.visible)
+          return "On"
+        else
+          return "Off"
+    }
 
 });
